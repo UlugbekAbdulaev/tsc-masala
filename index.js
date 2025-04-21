2c018a16bef15adc3fa0baa90c89dbe5663909b1
ulugbekAbdullayev <ulu23503@gmail.com>
2025-04-21 11:22:04 +0500

    +

--- index.js
+++ index.js
@@ -1,103 +1,169 @@
-// function t(a:number , b:number){
-//   let result = a+b*2
-//   console.log(result);
+// // function t(a:number , b:number){
+// //   let result = a+b*2
+// //   console.log(result);
+// // }
+// // t(15 , 20)
+// //1chi masala
+// function s(a: number): void {
+//     let p = 4 * a
+//     console.log("kvadrat peremetri " + p);
 // }
-// t(15 , 20)
-//1chi masala
-function s(a) {
-    var p = 4 * a;
-    console.log("kvadrat peremetri " + p);
+// s(10)
+// //2-masala
+// function kvYuzasi(a: number): any {
+//     let result = a ** 2
+//     console.log("kvadratning yuzi " + result);
+// }
+// kvYuzasi(5)
+// //3-masala
+// function YuzaPerimetr(a: number, b: number): void {
+//     const S = a * b;
+//     const P = 2 * (a + b);
+//     console.log(`Yuza: ${S}`, `Perimetr: ${P}`);
+//     console.log();
+// }
+// YuzaPerimetr(3, 5)
+// //4masala
+// function AylanaDiametri(d: number) {
+//     const length = d * Math.PI
+//     const uzunlik = length.toFixed(2)
+//     console.log(`Aylana uzunligi: ${uzunlik}`);
+// }
+// AylanaDiametri(4)
+// //5-masala
+// function KubYonTomoni(a: number) {
+//     const v = a ** 3
+//     const s = 6 * a ** 2
+//     console.log(`Hajmi: ${v}`, `to'la sirti: ${s}`);
+// }
+// KubYonTomoni(5)
+// //6masala
+// function paralapeped(a: number, b: number, c: number) {
+//     const v = a * b * c
+//     const s = 2 * (a * b + b * c + a * c)
+//     console.log(`Hajmi: ${v}`, `to'la sirti: ${s}`);
+// }
+// paralapeped(5, 7, 8)
+// //7-masala
+// function radius(r: number) {
+//     const L = 2 * Math.PI * r
+//     const S = Math.PI * r ** 2
+//     let uzunlik = L.toFixed(2)
+//     let radius = S.toFixed(2)
+//     console.log(`uzunligi: ${uzunlik}, Yuzasi: ${radius}`);
+// }
+// radius(3)
+// //8-masala
+// function arifmetik(a: number, b: number) {
+//     let j = (a + b) / 2
+//     console.log(`o'rta arifmetigi: ${j}`);
+// }
+// arifmetik(3, 4)
+// //9-masala
+// function geomeri(a: number, b: number) {
+//     if (a > 0 && b > 0) {
+//         let j = Math.sqrt(a * b)
+//         const result = j.toFixed(2)
+//         console.log(`o'rta geometrigi" ${result}`);
+//     } else {
+//         console.error("musbat son bo'lishi kerak");
+//     }
+// }
+// geomeri(6, 7)
+// // 10-masala
+// function yigKop(a: number, b: number) {
+//     if (a !== 0 && b !== 0) {
+//         let yig = a + b
+//         let kop = a * b
+//         let akvd = a ** 2
+//         let bkvd = b ** 2
+//         console.log(`Yig'indisi: ${yig}, Ko'paytmasi: ${kop}, a ning kvadrati: ${akvd}, b ning kvadrati: ${bkvd}`);
+//     } else {
+//         console.error("Ikkala son ham 0 ga teng bo'lmasligi kerak");
+//     }
+// } yigKop(4, 5)
+// //11-masala
+// function modul(a:number , b:number):void{
+//     let yig = a + b
+//     let kop = a * b
+//     let modulA = Math.abs(a) 
+//     let modulB = Math.abs(b) 
+//     console.log(`Yig'indisi: ${yig}, Ko'paytmasi: ${kop}, a ning moduli: ${modulA}, b ning moduli: ${modulB}`);
+// }
+// modul(2,3)
+// //12-masala 
+// function gipatinuza(a: number, b: number): void {
+//     const c = Math.sqrt(a ** 2 + b ** 2);
+//     const p = a + b + c;
+//     console.log(`Gipotenuza: ${c.toFixed(2)}, Peremetr: ${p.toFixed(2)}`);
+// }
+// gipatinuza(3, 4);
+////1
+var ism = "Ulugbek";
+var age = 30;
+console.log(ism + ' ' + age);
+///////2
+var isStudent = true;
+console.log(isStudent);
+//////3
+var meva = ["banana", "cherry"];
+console.log(meva);
+/////// Funksiyalar — types, default values, optional parameters
+//////1
+function qoshish(a, b) {
+    var result = a + b;
+    console.log(result);
 }
-s(10);
-//2-masala
-function kvYuzasi(a) {
-    var result = Math.pow(a, 2);
-    console.log("kvadratning yuzi " + result);
+qoshish(4, 8);
+/////////2
+function greet(name) {
+    if (name === void 0) { name = "Guest"; }
+    return "hello ".concat(name);
 }
-kvYuzasi(5);
-//3-masala
-function YuzaPerimetr(a, b) {
-    var S = a * b;
-    var P = 2 * (a + b);
-    console.log("Yuza: ".concat(S), "Perimetr: ".concat(P));
-    console.log();
-}
-YuzaPerimetr(3, 5);
-//4masala
-function AylanaDiametri(d) {
-    var length = d * Math.PI;
-    var uzunlik = length.toFixed(2);
-    console.log("Aylana uzunligi: ".concat(uzunlik));
-}
-AylanaDiametri(4);
-//5-masala
-function KubYonTomoni(a) {
-    var v = Math.pow(a, 3);
-    var s = 6 * Math.pow(a, 2);
-    console.log("Hajmi: ".concat(v), "to'la sirti: ".concat(s));
-}
-KubYonTomoni(5);
-//6masala
-function paralapeped(a, b, c) {
-    var v = a * b * c;
-    var s = 2 * (a * b + b * c + a * c);
-    console.log("Hajmi: ".concat(v), "to'la sirti: ".concat(s));
-}
-paralapeped(5, 7, 8);
-//7-masala
-function radius(r) {
-    var L = 2 * Math.PI * r;
-    var S = Math.PI * Math.pow(r, 2);
-    var uzunlik = L.toFixed(2);
-    var radius = S.toFixed(2);
-    console.log("uzunligi: ".concat(uzunlik, ", Yuzasi: ").concat(radius));
-}
-radius(3);
-//8-masala
-function arifmetik(a, b) {
-    var j = (a + b) / 2;
-    console.log("o'rta arifmetigi: ".concat(j));
-}
-arifmetik(3, 4);
-//9-masala
-function geomeri(a, b) {
-    if (a > 0 && b > 0) {
-        var j = Math.sqrt(a * b);
-        var result = j.toFixed(2);
-        console.log("o'rta geometrigi\" ".concat(result));
+greet("Sheroz");
+////////3
+function logInfo(ism, age) {
+    if (age) {
+        console.log("Ali is ".concat(age, " years old "));
     }
     else {
-        console.error("musbat son bo'lishi kerak");
+        console.log("Ali's age is unknown");
     }
 }
-geomeri(6, 7);
-// 10-masala
-function yigKop(a, b) {
-    if (a !== 0 && b !== 0) {
-        var yig = a + b;
-        var kop = a * b;
-        var akvd = Math.pow(a, 2);
-        var bkvd = Math.pow(b, 2);
-        console.log("Yig'indisi: ".concat(yig, ", Ko'paytmasi: ").concat(kop, ", a ning kvadrati: ").concat(akvd, ", b ning kvadrati: ").concat(bkvd));
+logInfo("ulugbek", 30);
+var Person1 = {
+    name: "Ali",
+    age: 25,
+    isStudent: true
+};
+function printCarInfo(car) {
+    console.log("Brand: ".concat(car.brand));
+    console.log("Year: ".concat(car.year));
+    console.log("Is Electric: ".concat(car.isElectric ? 'Yes' : 'No'));
+}
+//////Union Types, Literal Types, va Type Narrowing
+/////1
+function printId(id) {
+    if (typeof id === "string") {
+        console.log("ID is a string: ".concat(id));
     }
     else {
-        console.error("Ikkala son ham 0 ga teng bo'lmasligi kerak");
+        console.log("ID is a number: ".concat(id));
     }
 }
-yigKop(4, 5);
-//11-masala
-function modul(a, b) {
-    var yig = a + b;
-    var kop = a * b;
-    var modulA = Math.abs(a);
-    var modulB = Math.abs(b);
-    console.log("Yig'indisi: ".concat(yig, ", Ko'paytmasi: ").concat(kop, ", a ning moduli: ").concat(modulA, ", b ning moduli: ").concat(modulB));
+function move(dir) {
+    console.log("Moving ".concat(dir));
 }
-modul(2, 3);
-//12-masala 
-function gipatinuza(a, b) {
-    var c = Math.sqrt(Math.pow(a, 2) + Math.pow(b, 2));
-    var p = a + b + c;
-    console.log("Gipotenuza: ".concat(c.toFixed(2), ", Peremetr: ").concat(p.toFixed(2)));
+//////3
+function processItems(items) {
+    if (typeof items[0] === "string") {
+        items.forEach(function (item) {
+            console.log(item.toUpperCase());
+        });
+    }
+    else {
+        items.forEach(function (item) {
+            console.log(item * 2);
+        });
+    }
 }
-gipatinuza(3, 4);
--- index.ts
+++ index.ts
@@ -1,132 +1,235 @@
-// function t(a:number , b:number){
-//   let result = a+b*2
+// // function t(a:number , b:number){
+// //   let result = a+b*2
 
-//   console.log(result);
+// //   console.log(result);
+// // }
+// // t(15 , 20)
+
+
+// //1chi masala
+// function s(a: number): void {
+//     let p = 4 * a
+//     console.log("kvadrat peremetri " + p);
+
 // }
-// t(15 , 20)
+// s(10)
+
+// //2-masala
+// function kvYuzasi(a: number): any {
+//     let result = a ** 2
+//     console.log("kvadratning yuzi " + result);
+
+// }
+// kvYuzasi(5)
 
 
-//1chi masala
-function s(a: number): void {
-    let p = 4 * a
-    console.log("kvadrat peremetri " + p);
+// //3-masala
+// function YuzaPerimetr(a: number, b: number): void {
+//     const S = a * b;
+//     const P = 2 * (a + b);
 
+//     console.log(`Yuza: ${S}`, `Perimetr: ${P}`);
+//     console.log();
+// }
+// YuzaPerimetr(3, 5)
+
+
+// //4masala
+// function AylanaDiametri(d: number) {
+//     const length = d * Math.PI
+//     const uzunlik = length.toFixed(2)
+//     console.log(`Aylana uzunligi: ${uzunlik}`);
+
+// }
+// AylanaDiametri(4)
+
+// //5-masala
+// function KubYonTomoni(a: number) {
+//     const v = a ** 3
+//     const s = 6 * a ** 2
+//     console.log(`Hajmi: ${v}`, `to'la sirti: ${s}`);
+
+// }
+// KubYonTomoni(5)
+
+// //6masala
+// function paralapeped(a: number, b: number, c: number) {
+//     const v = a * b * c
+//     const s = 2 * (a * b + b * c + a * c)
+//     console.log(`Hajmi: ${v}`, `to'la sirti: ${s}`);
+// }
+// paralapeped(5, 7, 8)
+
+// //7-masala
+// function radius(r: number) {
+//     const L = 2 * Math.PI * r
+//     const S = Math.PI * r ** 2
+//     let uzunlik = L.toFixed(2)
+//     let radius = S.toFixed(2)
+//     console.log(`uzunligi: ${uzunlik}, Yuzasi: ${radius}`);
+
+// }
+// radius(3)
+
+// //8-masala
+// function arifmetik(a: number, b: number) {
+//     let j = (a + b) / 2
+//     console.log(`o'rta arifmetigi: ${j}`);
+
+// }
+// arifmetik(3, 4)
+
+// //9-masala
+// function geomeri(a: number, b: number) {
+//     if (a > 0 && b > 0) {
+//         let j = Math.sqrt(a * b)
+//         const result = j.toFixed(2)
+//         console.log(`o'rta geometrigi" ${result}`);
+//     } else {
+//         console.error("musbat son bo'lishi kerak");
+
+//     }
+
+// }
+// geomeri(6, 7)
+
+// // 10-masala
+// function yigKop(a: number, b: number) {
+//     if (a !== 0 && b !== 0) {
+//         let yig = a + b
+//         let kop = a * b
+//         let akvd = a ** 2
+//         let bkvd = b ** 2
+
+//         console.log(`Yig'indisi: ${yig}, Ko'paytmasi: ${kop}, a ning kvadrati: ${akvd}, b ning kvadrati: ${bkvd}`);
+//     } else {
+//         console.error("Ikkala son ham 0 ga teng bo'lmasligi kerak");
+//     }
+
+// } yigKop(4, 5)
+
+// //11-masala
+// function modul(a:number , b:number):void{
+//     let yig = a + b
+//     let kop = a * b
+//     let modulA = Math.abs(a) 
+//     let modulB = Math.abs(b) 
+
+//     console.log(`Yig'indisi: ${yig}, Ko'paytmasi: ${kop}, a ning moduli: ${modulA}, b ning moduli: ${modulB}`);
+// }
+// modul(2,3)
+
+
+// //12-masala 
+// function gipatinuza(a: number, b: number): void {
+//     const c = Math.sqrt(a ** 2 + b ** 2);
+//     const p = a + b + c;
+
+//     console.log(`Gipotenuza: ${c.toFixed(2)}, Peremetr: ${p.toFixed(2)}`);
+
+// }
+
+// gipatinuza(3, 4);
+
+
+
+////1
+let ism: string = "Ulugbek"
+let age: number = 30
+console.log(ism + ' ' + age);
+
+///////2
+let isStudent: boolean = true
+console.log(isStudent);
+
+//////3
+let meva: Array<String> = ["banana", "cherry"]
+console.log(meva);
+
+/////// Funksiyalar — types, default values, optional parameters
+//////1
+function qoshish(a: number, b: number) {
+    let result = a + b
+    console.log(result);
 }
-s(10)
+qoshish(4, 8)
 
-//2-masala
-function kvYuzasi(a: number): any {
-    let result = a ** 2
-    console.log("kvadratning yuzi " + result);
-
+/////////2
+function greet(name: string = "Guest"): string {
+    return `hello ${name}`
 }
-kvYuzasi(5)
+greet("Sheroz")
 
+////////3
+function logInfo(ism: string, age?: number) {
+    if (age) {
+        console.log(`Ali is ${age} years old `);
 
-//3-masala
-function YuzaPerimetr(a: number, b: number): void {
-    const S = a * b;
-    const P = 2 * (a + b);
-
-    console.log(`Yuza: ${S}`, `Perimetr: ${P}`);
-    console.log();
-}
-YuzaPerimetr(3, 5)
-
-
-//4masala
-function AylanaDiametri(d: number) {
-    const length = d * Math.PI
-    const uzunlik = length.toFixed(2)
-    console.log(`Aylana uzunligi: ${uzunlik}`);
-
-}
-AylanaDiametri(4)
-
-//5-masala
-function KubYonTomoni(a: number) {
-    const v = a ** 3
-    const s = 6 * a ** 2
-    console.log(`Hajmi: ${v}`, `to'la sirti: ${s}`);
-
-}
-KubYonTomoni(5)
-
-//6masala
-function paralapeped(a: number, b: number, c: number) {
-    const v = a * b * c
-    const s = 2 * (a * b + b * c + a * c)
-    console.log(`Hajmi: ${v}`, `to'la sirti: ${s}`);
-}
-paralapeped(5, 7, 8)
-
-//7-masala
-function radius(r: number) {
-    const L = 2 * Math.PI * r
-    const S = Math.PI * r ** 2
-    let uzunlik = L.toFixed(2)
-    let radius = S.toFixed(2)
-    console.log(`uzunligi: ${uzunlik}, Yuzasi: ${radius}`);
-
-}
-radius(3)
-
-//8-masala
-function arifmetik(a: number, b: number) {
-    let j = (a + b) / 2
-    console.log(`o'rta arifmetigi: ${j}`);
-
-}
-arifmetik(3, 4)
-
-//9-masala
-function geomeri(a: number, b: number) {
-    if (a > 0 && b > 0) {
-        let j = Math.sqrt(a * b)
-        const result = j.toFixed(2)
-        console.log(`o'rta geometrigi" ${result}`);
     } else {
-        console.error("musbat son bo'lishi kerak");
+        console.log("Ali's age is unknown");
 
     }
-
 }
-geomeri(6, 7)
+logInfo("ulugbek", 30)
 
-// 10-masala
-function yigKop(a: number, b: number) {
-    if (a !== 0 && b !== 0) {
-        let yig = a + b
-        let kop = a * b
-        let akvd = a ** 2
-        let bkvd = b ** 2
 
-        console.log(`Yig'indisi: ${yig}, Ko'paytmasi: ${kop}, a ning kvadrati: ${akvd}, b ning kvadrati: ${bkvd}`);
+///////Objectlar va Type Aliaslar
+//////1
+type Person = {
+    name: string;
+    age: number;
+    isStudent: boolean
+};
+
+const Person1: Person = {
+    name: "Ali",
+    age: 25,
+    isStudent: true
+};
+
+
+//////2
+type Car = {
+    brand: string;
+    year: number;
+    isElectric: boolean;
+};
+
+function printCarInfo(car: Car): void {
+    console.log(`Brand: ${car.brand}`);
+    console.log(`Year: ${car.year}`);
+    console.log(`Is Electric: ${car.isElectric ? 'Yes' : 'No'}`);
+}
+
+
+//////Union Types, Literal Types, va Type Narrowing
+/////1
+function printId(id: string | number): void {
+    if (typeof id === "string") {
+        console.log(`ID is a string: ${id}`);
     } else {
-        console.error("Ikkala son ham 0 ga teng bo'lmasligi kerak");
+        console.log(`ID is a number: ${id}`);
     }
-
-} yigKop(4, 5)
-
-//11-masala
-function modul(a:number , b:number):void{
-    let yig = a + b
-    let kop = a * b
-    let modulA = Math.abs(a) 
-    let modulB = Math.abs(b) 
-
-    console.log(`Yig'indisi: ${yig}, Ko'paytmasi: ${kop}, a ning moduli: ${modulA}, b ning moduli: ${modulB}`);
-}
-modul(2,3)
-
-
-//12-masala 
-function gipatinuza(a: number, b: number): void {
-    const c = Math.sqrt(a ** 2 + b ** 2);
-    const p = a + b + c;
-
-    console.log(`Gipotenuza: ${c.toFixed(2)}, Peremetr: ${p.toFixed(2)}`);
-    
 }
 
-gipatinuza(3, 4);
+
+//////2
+type Direction = 'up' | 'down' | 'left' | 'right';
+
+function move(dir: Direction): void {
+    console.log(`Moving ${dir}`);
+}
+
+
+//////3
+function processItems(items: string[] | number[]): void {
+    if (typeof items[0] === "string") {
+        (items as string[]).forEach(item => {
+            console.log(item.toUpperCase());
+        });
+    } else {
+        (items as number[]).forEach(item => {
+            console.log(item * 2);
+        });
+    }
+}
